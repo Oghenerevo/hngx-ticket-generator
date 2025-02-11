be5f99e29309190c50f8885be342109cc82d244a
Justfemi <ajibade.olufemi@yahoo.com>
2025-02-11 06:15:46 +0100

    feat: added ticket setp1 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header';
+import Ticket from './components/Ticket';
 
 function App() {
 
   return (
     <main>
       <Header />
+      <Ticket />
     </main>
   )
 }
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Ticket = () => {
+		return (
+				<section className="container">
+					<div className="heading">
+						<h2>Ticket Selection</h2>
+						<p>Step 1/3</p>
+					</div>
+					{/* progress bar */}
+
+					<div className="inner-wrapper">
+						<div className="top-details">
+							<h1>Techember Fest ”25</h1>
+							<p>
+								Join us for an unforgettable experience at [Event Name]! Secure your spot now.
+							</p>
+							<div className="event-details">
+								<p>📍 [Event Location] || March 15, 2025 | 7:00 PM </p>
+							</div>
+						</div>
+
+						<div className="line"></div>
+
+						<div>
+							<p>Select 
+						</div>
+					</div>
+				</section>
+		)
+}
+
+export default Ticket

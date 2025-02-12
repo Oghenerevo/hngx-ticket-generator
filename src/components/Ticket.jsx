cdc00cee1974164182af36fdbcb05c807b50293b
Justfemi <ajibade.olufemi@yahoo.com>
2025-02-13 00:21:55 +0100

    fix: recovered files

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,5 +1,6 @@
-import { ImagePlus, Mail } from 'lucide-react';
+import { Mail } from 'lucide-react';
 import { useState } from 'react'
+import imgUpload from "../assets/upload.svg";
 
 const Ticket = () => {
 	const [selectedNumber, setSelectedNumber] = useState("");
@@ -27,90 +28,78 @@ const Ticket = () => {
 				{step === 1 && (
 					<>
 						<div className="heading">
-						<h2>Ticket Selection</h2>
-						<p>{step}/3</p>
-					</div>
-	
-					<div className="progress-container">
-							<div
-									className="progress-bar"
-									style={{ width: `${(step / 3) * 100}%`, backgroundColor: "#24A0B5" }}
-							></div>
-					</div>
-					<div className="inner-wrapper">
-						<div className="top-details">
-							<h1>Techember Fest ”25</h1>
-							<p>
-								Join us for an unforgettable experience at [Event Name]! Secure your spot now.
-							</p>
-							<div className="event-details">
-								<p>📍 [Event Location] || March 15, 2025 | 7:00 PM </p>
-							</div>
+							<h2>Ticket Selection</h2>
+							<p>{step}/3</p>
 						</div>
-
-						<div className="line"></div>
-
-						<div className="ticket-section">
-							<p>Select Ticket Type: </p>
-								<div className="ticket-types">
-									<div className="ticket">
-										<div className="type">
-											<h3>Regular access</h3>
-											<p>20 left!</p>
-										</div>
-										<div className="amount">
-											<h3>Free</h3>
-										</div>
-									</div>
-
-									<div className="ticket">
-										<div className="type">
-											<h3>vip access</h3>
-											<p>20 left!</p>
-										</div>
-			
-										<div className="amount">
-											<h3>$50</h3>
-										</div>
-									</div>
-
-									<div className="ticket">
-										<div className="type">
-											<h3>vvip access</h3>
-											<p>20 left!</p>
-										</div>
-										<div className="amount">
-											<h3>$150</h3>
-										</div>
-									</div>
+						<div className="progress-container">
+								<div
+										className="progress-bar"
+										style={{ width: `${(step / 3) * 100}%`, backgroundColor: "#24A0B5" }}
+								></div>
+						</div>
+						<div className="inner-wrapper">
+							<div className="top-details">
+								<h1>Techember Fest ”25</h1>
+								<p>
+									Join us for an unforgettable experience at [Event Name]! Secure your spot now.
+								</p>
+								<div className="event-details">
+									<p>
+										📍 [Event Location]
+										<span>| |</span>
+									</p>
+									<p className="date">March 15, 2025 | 7:00 PM</p>
 								</div>
-						</div>
+							</div>
 
-						<div className="ticket-section">
-							<p>Number of Ticket: </p>
-							<select
-									id="numberDropdown"
-									value={selectedNumber}
-									onChange={(e) => setSelectedNumber(e.target.value)}
-									className='drop-down'
-							>
-									<option value="" disabled>Choose number of ticket(s)</option>
-									{[...Array(10)].map((_, index) => (
-											<option key={index + 1} value={index + 1}>
-													{index + 1}
-											</option>
-									))}
-							</select>
-						</div>
+							<div className="line"></div>
 
-						<div className="bottom-btns">
-							<div>
+							<div className="ticket-section">
+								<p>Select Ticket Type: </p>
+									<div className="ticket-types">
+										<div className="ticket">
+											<h2>Free</h2>
+											<h4>Regular access</h4>
+											<p>20/52</p>
+										</div>
+
+										<div className="ticket">
+											<h2>$50</h2>
+											<h4>vip access</h4>
+											<p>20/52</p>
+										</div>
+
+										<div className="ticket">
+											<h2>$150</h2>
+											<h4>vvip access</h4>
+											<p>20/52</p>
+										</div>
+									</div>
+							</div>
+
+							<div className="ticket-section">
+								<p>Number of Ticket: </p>
+								<select
+										id="numberDropdown"
+										value={selectedNumber}
+										onChange={(e) => setSelectedNumber(e.target.value)}
+										className='drop-down'
+								>
+										<option value="" disabled>Number of ticket(s)</option>
+										{[...Array(10)].map((_, index) => (
+												<option key={index + 1} value={index + 1}>
+														{index + 1}
+												</option>
+										))}
+								</select>
+							</div>
+
+							<div className="btns">
 								<button className="btn cancel" onClick={handleBack} disabled={step === 1}>Cancel</button>
 
 								<button className="btn next" onClick={handleNext} disabled={step === 3}>Next</button>
 							</div>
 						</div>
-					</div>
 					</>
 				)}
 
@@ -134,7 +123,7 @@ const Ticket = () => {
 								<div className="darker">
 									<div className="image-holder">
 										<div className="content">
-											<ImagePlus className="upload-icon"/>
+											<img src={imgUpload} alt="img-upload icon" className="upload-icon"/>
 											<p>Drag & drop or click to upload</p>
 										</div>
 									</div>
@@ -158,10 +147,10 @@ const Ticket = () => {
 
 							<div className="input-container">
 								<label htmlFor="About">About the project</label>
-								<textarea name="About"></textarea>
+								<textarea name="About" placeholder="Textarea"></textarea>
 							</div>
 					
-							<div>
+							<div className="btns">
 								<button className="btn cancel" onClick={handleBack} disabled={step === 1}>Back</button>
 
 								<button className="btn next" onClick={handleNext} disabled={step === 3}>Get My Ticket</button>
@@ -184,18 +173,21 @@ const Ticket = () => {
 								></div>
 						</div>
 
-						<div className="inner-wrapper">
-							<div className="top-details">
+						<div className="">
+							<div className='ticket-title'>
+								<h1>Your Ticket is Booked!</h1>
+								<p>You can download or Check your email for a copy</p>
+							</div>
+
+							<div className="event-ticket">
 
 							</div>
 					
-							<div className="line"></div>
-					
-							<div>
-								<button className="btn cancel" onClick={handleNewTicket}>Book Another Ticket</button>
+								<div className="btns">
+									<button className="btn cancel" onClick={handleNewTicket}>Book Another Ticket</button>
 
-								<button className="btn next">Download Ticket</button>
-							</div>
+									<button className="btn next">Download Ticket</button>
+								</div>
 						</div>
 					</>
 				)}
